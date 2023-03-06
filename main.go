c74e6a4f007eb7b981270eecd955c55ea7c65325
iamdevtry <tranbinhphongcmg@gmail.com>
2023-03-07 00:03:36 +0700

    update .gitignore file

--- main.go
+++ main.go
@@ -8,14 +8,22 @@ import (
 	_ "github.com/sijms/go-ora/v2"
 )
 
+const createTableStatement = "CREATE TABLE TEMP_TABLE ( NAME VARCHAR2(100), CREATION_TIME TIMESTAMP DEFAULT SYSTIMESTAMP, VALUE  NUMBER(5))"
+
 func main() {
 	fmt.Println("Hello, World!")
-	connectionString := "oracle://" + "system" + ":" + "123456@@" + "@" + "localhost" + ":" + "1521" + "/" + "orcl"
+
 	db, err := sql.Open("oracle", connectionString)
 
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	_, err = db.Exec(createTableStatement)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	defer db.Close()
 
 	fmt.Println("Connected to Oracle Database")
