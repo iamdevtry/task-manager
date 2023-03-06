cb89d55976861f272890b9a58a5c2556fe5f6cda
iamdevtry <tranbinhphongcmg@gmail.com>
2023-03-06 19:05:46 +0700

    add go-ora

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/iamdevtry/task-manager
 
 go 1.20
+
+require github.com/sijms/go-ora/v2 v2.5.33
--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	_ "github.com/sijms/go-ora/v2"
+)
 
 func main() {
 	fmt.Println("Hello, World!")
+	connectionString := "oracle://" + "system" + ":" + "123456@@" + "@" + "localhost" + ":" + "1521" + "/" + "orcl"
+	db, err := sql.Open("oracle", connectionString)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	fmt.Println("Connected to Oracle Database")
 }
